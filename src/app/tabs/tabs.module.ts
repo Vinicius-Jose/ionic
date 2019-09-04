a81c9184d17592c2b0831901fa42ffebaf4ca756
Vinícius <vinijosenog@hotmail.com>
2019-09-04 13:29:57 -0300

    first commit

---
+++ src/app/tabs/tabs.module.ts
@@ -0,0 +1,54 @@
+import { IonicModule } from '@ionic/angular';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TabsPageRoutingModule } from './tabs.router.module';
+
+import { TabsPage } from './tabs.page';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        children: [
+          {
+            path: '',
+            loadChildren: '../tab1/tab1.module#Tab1PageModule'
+          }
+        ]
+      },
+      {
+        path: 'feed',
+        children: [
+          {
+            path: '',
+            loadChildren: '../feed/feed.module#FeedPageModule'
+          }
+        ]
+      },
+	  {
+        // Pasta DEFAULT
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full'
+      } 
+	 ]
+	}
+  ];
+
+@NgModule({
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    TabsPageRoutingModule
+  ],
+  declarations: [TabsPage]
+})
+export class TabsPageModule {}
---
+++ src/app/tabs/tabs.router.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        children: [
+          {
+            path: '', 
+            loadChildren: () =>
+              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
+          }
+        ]
+      },
+	  {
+        path: 'feed',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../feed/feed.module').then(m => m.FeedPageModule)
+          }
+        ]
+      },
+	  
+      {
+        path: '',
+        redirectTo: '/tabs/tab1',
+        pathMatch: 'full'
+      }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/tabs/tab1',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
